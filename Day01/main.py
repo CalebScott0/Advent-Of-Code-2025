983c06fefb0fd752ba984d0156c5425bad1b8667
CalebScott0 <calebbenscott@gmail.com>
2025-12-04 21:18:47 -0600

    Advent of code 2025 day 1

---
+++ Day01/main.py
@@ -0,0 +1,38 @@
+dialPosition = 50
+numberZeros = 0;
+
+iters = 0;
+
+with open("../Combos.txt", "r") as f:
+    for line in f:
+        iters += 1
+        # L or R
+        direction = line[0] # STRING
+
+        # % 100 gives one rotation distance
+        value = int(line[1:]) % 100         
+        
+        # 100 needed as offset for 0
+        if direction == "L":
+            if value <= dialPosition:
+                dialPosition -= value
+            else:
+                dialPosition = 100 - (value - dialPosition)
+        else:
+            if value <= (99 - dialPosition):
+                dialPosition += value
+            else:
+                dialPosition = value - (100 - dialPosition)
+
+        if (int(dialPosition) == 0):
+            numberZeros += 1;
+        
+
+print("Number Zeros: ", numberZeros)
+
+
+
+
+        
+
+
