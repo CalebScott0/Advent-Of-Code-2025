376f36a39e63cedaa032dd7110eda5f1878aee96
CalebScott0 <calebbenscott@gmail.com>
2025-12-05 18:34:23 -0600

    Advent of code 2025 day 2

--- Day02.py
+++ Day02.py
@@ -1,44 +1,45 @@
-dialPosition = 50 
+dialPosition = 50
+numberZeros = 0
 
-numberZeros = 0;
+x = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"]
 
+iters = 0
 with open("Combos.txt", "r") as f:
     for line in f:
-
+    #for line in x:
         # L or R
         direction = line[0] # STRING
+        value = int(line.strip()[1:])
 
-        value = int(line[1:])
-
-        if (int(dialPosition) == 0):
-            numberZeros += 1;
-
-        if value > 100:
-            numberZeros += int(value / 100)
+        # % 100 gives one rotation distance
+        if value >= 100:
+            numberZeros += value // 100
             value %= 100
-
-        if direction == "L":
-            if value <= dialPosition:
-                dialPosition -= value
-            else:
-                if (int(dialPosition) != 0 and 100 - (value - dialPosition) != 0):
-                    numberZeros += 1;
-                dialPosition = 100 - (value - dialPosition)
-        else:
-            if value <= (100 - dialPosition):
-                dialPosition += value
-            else:
-                if (int(dialPosition) != 0 and value - (100 - dialPosition) != 0):
-                    numberZeros += 1;
-                dialPosition = value - (100 - dialPosition)
-
         
+        if direction == "L":
+            if value == dialPosition:
+                dialPosition = 0
+                numberZeros += 1
+
+            elif value > dialPosition:
+                if not dialPosition == 0:
+                    numberZeros += 1
+                value = value - dialPosition
+                dialPosition = 100 - value
+
+            else:
+                dialPosition -= value
+        else:
+            if value == (100 - dialPosition):
+                numberZeros += 1
+                dialPosition = 0
+
+            elif value > (100 - dialPosition):
+                numberZeros += 1
+                dialPosition = (value - (100 - dialPosition))
+
+            else: 
+                dialPosition += value
+
 
 print("Number Zeros: ", numberZeros)
-
-
-
-
-        
-
-
